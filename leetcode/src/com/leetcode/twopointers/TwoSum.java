e792b711b6b8898b6d67452b679e386cfd25e09d
mr-arora30 <36749276+mr-arora30@users.noreply.github.com>
2021-08-25 08:23:48 +0000

    two pointers added

---
+++ leetcode/src/com/leetcode/twopointers/TwoSum.java
@@ -0,0 +1,43 @@
+/*
+Problem Statement#
+Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
+
+Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+
+Example 1:
+
+Input: [1, 2, 3, 4, 6], target=6
+Output: [1, 3]
+Explanation: The numbers at index 1 and 3 add up to 6: 2+4=6
+Example 2:
+
+Input: [2, 5, 9, 11], target=11
+Output: [0, 2]
+Explanation: The numbers at index 0 and 2 add up to 11: 2+9=11
+*/
+
+import java.util.*;
+class TwoSum{
+
+  public static void main(String args[]){
+     int arr[] = {2, 5, 9, 11};
+     int targetSum=11;
+     System.out.println(Arrays.toString(search(arr, targetSum)));
+  }
+
+   public static int[] search(int[] arr, int targetSum) {   
+
+     int left = 0, right = arr.length - 1;
+    while (left < right) {
+      int currentSum = arr[left] + arr[right];
+      if (currentSum == targetSum)
+        return new int[] { left, right }; // found the pair
+
+      if (targetSum > currentSum)
+        left++; // we need a pair with a bigger sum
+      else
+        right--; // we need a pair with a smaller sum
+    }
+    return new int[] { -1, -1 };
+  }
+}
