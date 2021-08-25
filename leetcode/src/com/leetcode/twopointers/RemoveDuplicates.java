e6e83b5b8945a364857c98a78dc71fe51cce7476
mr-arora30 <36749276+mr-arora30@users.noreply.github.com>
2021-08-25 09:28:55 +0000

    added remove duplicates in a sorted array

---
+++ leetcode/src/com/leetcode/twopointers/RemoveDuplicates.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class RemoveDuplicates { 
+  public static void main(String args[]){
+     int arr[] ={2, 3, 3, 3, 6, 9, 9};
+    System.out.println(remove(arr));
+  }
+
+/*
+Given an array of sorted numbers, remove all duplicates from it. You should not use any extra space; after removing the duplicates in-place return the length of the subarray that has no duplicate in it.
+
+Example 1:
+
+Input: [2, 3, 3, 3, 6, 9, 9]
+Output: 4
+Explanation: The first four elements after removing the duplicates will be [2, 3, 6, 9].
+Example 2:
+
+Input: [2, 2, 2, 11]
+Output: 2
+Explanation: The first two elements after removing the duplicates will be [2, 11].
+*/
+/*
+As the input array is sorted, therefore, one way to do this is to shift the elements left whenever we encounter duplicates. In other words, we will keep one pointer for iterating the array and one pointer for placing the next non-duplicate number. So our algorithm will be to iterate the array and whenever we see a non-duplicate number we move it next to the last non-duplicate number we’ve seen.
+*/
+  public static int remove(int[] arr) {
+   int nextNonDuplicate=1; // index of the next non-duplicate element
+   for(int i=1;i<arr.length;i++){    
+      if(arr[nextNonDuplicate-1]!=arr[i]){
+        arr[nextNonDuplicate]=arr[i];
+            nextNonDuplicate++;
+      }
+  
+   }
+   return nextNonDuplicate;
+  }
+}
